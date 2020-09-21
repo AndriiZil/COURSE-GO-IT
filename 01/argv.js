8e91ec667a97694a8a3b03986e653af74f211bb5
Andrii Zilnyk <andrii.zilnyk@gmail.com>
2020-09-21 23:29:23 +0300

    fixed code

--- 01/argv.js
+++ 01/argv.js
@@ -2,8 +2,10 @@ const [operator, ...numbers] = process.argv.slice(2);
 
 
 switch (operator) {
-    case 'add': 
+    case 'add':
         console.log(numbers.reduce((acc, cur) => acc + parseFloat(cur), 0));
+        break;
     case 'sub':
         console.log(numbers.reduce((acc, cur) => parseFloat(acc) - parseFloat(cur)));
+        break;
 }
